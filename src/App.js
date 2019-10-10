4aadcafaa71e3bb2d88c1ee98ae66a8503c7f0b1
zxq08 <461890276@qq.com>
2019-10-10 18:21:57 +0800

    init

--- src/App.js
+++ src/App.js
@@ -7,18 +7,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>hello react! my demo!</h2>
       </header>
+      <p className="App-intro">
+        你可以在 <code>src/App.js</code> 文件中修改
+      </p>
     </div>
   );
 }
