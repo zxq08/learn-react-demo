2b3ea78821bb495a3fbdbd9cc7e4618afc16d6b3
zxq <461890276@qq.com>
2019-10-19 21:01:06 +0800

    'login'

---
+++ src/components/home/home_login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class HomeLogin extends React.Component {
+	constructor () {
+
+	}
+
+	render () {
+		return (
+			<div> it is my home-logined </div>
+		)
+	}
+}
--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,38 +1,78 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
-import './login.css';
+import React from 'react'
+import axios from 'axios'
+import './login.css'
 import Clock from '../clock/clock'
 
-function Login (props) {
-	return (
-		<div className="login-wrap">
-	    <div className="login-body">
-	      <p>{props.systemName}</p>
-	      <div className="login-content">
-	        <input ref='username' type="text" placeholder="用户名" />
-	        <input ref='password' type="password" placeholder="密码" />
-	        <div className="login-btn" onClick={() => this.handleLogin()}>
-	        登录
-	        </div>
-	      </div>
-	      <div id="clockWrap">
-	      	<Clock />
-	      </div>
-	    </div>
-	  </div>
-	)
-}
+class Login extends React.Component {
 
-function handleLogin () {
-	let username = this.refs.username.value
-	let password = this.refs.password.value
-	this.setState({
-		username: username,
-		password: password
-	})
-	console.log('user:' + this.state.username)
-	console.log('pass:' + this.state.password)
-}
+	constructor (props) {
+		super(props)
+		this.state = {username: '', password: '', _username: '', _password: ''}
+		this.handleLogin = this.handleLogin.bind(this)
+		this.handleUserChange = this.handleUserChange.bind(this)
+		this.handlePassChange = this.handlePassChange.bind(this)
+	}
+	
+	componentDidMount () {
+		axios.get('/static/mock/user.json', {})
+			.then((res) => {
+				// console.log(this) 此时this指向当前组件
+				res = res.data
+				if (res.errno === '0') {
+					this.setState({
+						_username: res.data.username,
+						_password: res.data.password
+					})
+				} else {
+					console.log('json err:' + res.errno)
+				}
+			})
+			.catch(function (err) {
+				console.log("axios err:" + err)
+			})
+	}
 
+	handleLogin (event) {
+		// onSubmit中不能使用箭头函数，会导致this指向改变，取不到event
+		event.preventDefault()
+		let username = this.state.username
+		let password = this.state.password
+		console.log(username + ',' + password)
+		if (username && password) {
+			if (username === this.state._username && password === this.state._password) {
+				// window.location = "https://www.baidu.com"
+				window.location = "../home/home_login"
+			}
+		} else {
+			alert('用户名或密码不能为空')
+		}
+	}
+
+	handleUserChange (event) {
+		this.setState({username: event.target.value})
+	}
+
+	handlePassChange (event) {
+		this.setState({password: event.target.value})
+	}
+
+	render () {
+		return (
+			<div className="login-wrap">
+		    <div className="login-body">
+		      <p>{this.props.systemName}</p>
+		      <form className="login-content" onSubmit={this.handleLogin}>
+		        <input value={this.state.username} type="text" placeholder="用户名" onChange={this.handleUserChange} />
+		        <input value={this.state.password} type="password" placeholder="密码" onChange={this.handlePassChange} />
+		        <input className="login-btn" type="submit" value="登录" />
+		      </form>
+		      <div id="clockWrap">
+		      	<Clock />
+		      </div>
+		    </div>
+		  </div>
+		)
+	}
+}
 
 export default Login
